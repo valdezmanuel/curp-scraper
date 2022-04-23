e7a35f7ae35277b7cc7f8a8d168513be80ca121f
removedporn <86824510+removedporn@users.noreply.github.com>
2022-04-23 11:39:51 +0800

    Delete example directory

--- example/index.js
+++
@@ -1,9 +0,0 @@
-const curpScraper = require('../dist/curp-scraper.js');
-
-curpScraper.getCurpDetails('REPLACE THIS WITH A VALID CURP', (error, response) => {
-    if (error) {
-        console.log("error", error);
-    } else {
-        console.log("response", response);
-    }   
-})
